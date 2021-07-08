7532d727f248530706eaa11c98333d7fdb4ff25e
s1280198 <87122111+s1280198@users.noreply.github.com>
2021-07-08 21:25:44 +0900

    Won/Lost message added

---
+++ Won_or_Lost.py
@@ -0,0 +1,15 @@
+import random
+
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+sum = dice1 + dice2
+
+print("Rolling the dice...")
+print("Die 1: " + str(dice1))
+print("Die 2: " + str(dice2))
+print("Total value: " + str(sum))
+
+if sum > 7:
+    print("You won")
+else:
+    print("You lost")
